89ff6dd291c86259beb71c95e61386abf1dce751
MUHMMAD SALEH <khanmohdsaleh2002@gmail.com>
2024-08-12 19:04:09 +0530

    added search Bar in navbar

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,21 +1,23 @@
 import { NavbarItem } from "./NavbarItem";
 import { NavbarItemData } from "../data/Navbar_Item";
+import { SearchBar } from "./SearchBar";
 
 export const Navbar = () => {
   return (
-    <div className="flex  w-full px-[10px] py-[15px] bg-[#27272F]  rounded-lg ">
+    <div className="flex items-center  w-full px-[10px] py-[15px] bg-[#27272F]  rounded-lg ">
       <div
         className="w-[40%] 
-text-[#FFFFFF]  font-[Rubik]"
+text-[#FFFFFF]  font-[Rubik] font-bold"
       >
         Design Studio
       </div>
 
-      <div className="w-[60%] flex justify-between ">
+      <div className="w-[60%] flex justify-between items-center ">
         {NavbarItemData.map((item) => (
           <NavbarItem icon={item.icon} label={item.label} />
         ))}
-        <div>search bar</div>
+        
+        <div><SearchBar/></div>
         <div>notification icon</div>
         <div>profile</div>
       </div>
--- src/component/SearchBar.tsx
+++ src/component/SearchBar.tsx
@@ -1,8 +1,8 @@
 export const SearchBar=()=>{
     return(
-        <div className="flex w-full items-center gap-[20px] bg-[#27272F]">
-            <div><input type="text" name="search" placeholder="Search Project...." /></div>
-            <div><img src="./assets/search-normal.svg" alt="icon" /></div>
+        <div className="flex  items-center gap-[20px] p-1 border border-[#C0C0D680] rounded-[10px] ">
+            <div ><input type="text" name="search" placeholder="Search Project...." className="inline-block bg-[#27272F] py-1 px-1 focus:outline-none text-[14px] font-[Rubik] font-normal" /></div>
+            <div className="border rounded-md w-[28px] p-1 border border-[#C0C0D680]"><img src="./assets/search-normal.svg" alt="icon" /></div>
         </div>
     )
 }
